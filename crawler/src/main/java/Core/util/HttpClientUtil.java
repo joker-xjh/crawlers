dc71eff32c7901c2d24bf1516d1b8f732c5a6c7b
joker_xjh <1781934063@qq.com>
2017-09-21 14:30:15 +0800

    to be continued

--- crawler/src/main/java/Core/util/HttpClientUtil.java
+++ crawler/src/main/java/Core/util/HttpClientUtil.java
@@ -7,20 +7,31 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-
+import java.net.UnknownHostException;
+import java.nio.charset.CodingErrorAction;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLException;
+
+import org.apache.http.Consts;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.config.RequestConfig.Builder;
@@ -30,12 +41,28 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
+@SuppressWarnings("deprecation")
 public class HttpClientUtil {
 	
 	private static Logger logger = SimpleLogger.getSimpleLogger(HttpClientUtil.class);
@@ -51,6 +78,107 @@ public class HttpClientUtil {
 	private static CloseableHttpClient httpClient;
 	
 	private static CookieStore cookieStore = new BasicCookieStore();
+	
+	static {
+		init();
+	}
+	
+	
+	private static void init() {
+		try {
+			SSLContext sslContext =SSLContexts.custom().loadTrustMaterial(KeyStore.getInstance(KeyStore.getDefaultType()),
+					new TrustStrategy() {
+						
+						@Override
+						public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+							return true;
+						}
+					}).build();
+			SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext);
+			Registry<ConnectionSocketFactory> socketFactoryRegistry  = RegistryBuilder.<ConnectionSocketFactory>create()
+					                                                   .register("http", PlainConnectionSocketFactory.INSTANCE)
+					                                                   .register("https", sslConnectionSocketFactory)
+					                                                   .build();
+			PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+			SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(Constants.TIMEOUT).setTcpNoDelay(true).build();
+			connectionManager.setDefaultSocketConfig(socketConfig);
+			ConnectionConfig connectionConfig = ConnectionConfig.custom().setMalformedInputAction(CodingErrorAction.IGNORE)
+					 .setUnmappableInputAction(CodingErrorAction.IGNORE).setCharset(Consts.UTF_8).build();
+			
+			connectionManager.setDefaultConnectionConfig(connectionConfig);
+			connectionManager.setMaxTotal(100);
+			connectionManager.setDefaultMaxPerRoute(50);
+			HttpRequestRetryHandler  handler = new HttpRequestRetryHandler() {
+				
+				@Override
+				public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
+					if (executionCount > 2) {
+						return false;
+					}
+					if (exception instanceof InterruptedIOException) {
+						return true;
+					}
+					if (exception instanceof ConnectTimeoutException) {
+						return true;
+					}
+					if (exception instanceof UnknownHostException) {
+						return true;
+					}
+					if (exception instanceof SSLException) {
+						return true;
+					}
+					HttpRequest request = HttpClientContext.adapt(context).getRequest();
+					if (!(request instanceof HttpEntityEnclosingRequest)) {
+						return true;
+					}
+					
+					
+					return false;
+				}
+			};
+			
+			HttpClientBuilder httpClientBuilder = HttpClients.custom().setConnectionManager(connectionManager)
+					                                                  .setRetryHandler(handler)
+					                                                  .setUserAgent(userAgent);
+			 if (proxy != null) {
+	                httpClientBuilder.setRoutePlanner(new DefaultProxyRoutePlanner(proxy)).build();
+	            }
+			 httpClient = httpClientBuilder.build();
+			 
+			 requestConfig = RequestConfig.custom().setSocketTimeout(Constants.TIMEOUT)
+					                               .setConnectTimeout(Constants.TIMEOUT)
+					                               .setConnectionRequestTimeout(Constants.TIMEOUT)
+					                               .setCookieSpec(CookieSpecs.STANDARD)
+					                               .build();
+			 
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Exception:", e);
+		} 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static String postRequest(String postUrl, Map<String, String> params) throws IOException {
+		HttpPost request = new HttpPost(postUrl);
+		setHttpPostParams(request, params);
+		return getWebPage(request, "UTF-8");
+	}
 	
 	
 	

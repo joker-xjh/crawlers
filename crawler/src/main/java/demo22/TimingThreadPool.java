d25c9b1bf00b88c255290c4258e523867dc99a26
joker_xjh <1781934063@qq.com>
2017-10-07 09:19:24 +0800

    TimingThreadPool

---
+++ crawler/src/main/java/demo22/TimingThreadPool.java
@@ -0,0 +1,53 @@
+package demo22;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+public class TimingThreadPool extends ThreadPoolExecutor{
+
+	public TimingThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
+			BlockingQueue<Runnable> workQueue) {
+		super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+	}
+	
+	private final ThreadLocal<Long> startTime = new ThreadLocal<>();
+	private final Logger logger = Logger.getLogger("TimingThreadPool");
+	private final AtomicLong numTasks = new AtomicLong();
+	private final AtomicLong totalTime = new AtomicLong();
+	
+	
+	@Override
+	protected void beforeExecute(Thread t, Runnable r) {
+		super.beforeExecute(t, r);
+		logger.fine(String.format("Thread : %s :  start: %s", t,r));
+		startTime.set(System.currentTimeMillis());
+	}
+	
+	@Override
+	protected void afterExecute(Runnable r, Throwable t) {
+		try {
+			long endTime = System.currentTimeMillis();
+			long taskTime = endTime - startTime.get();
+			numTasks.incrementAndGet();
+			totalTime.addAndGet(taskTime);
+		} finally {
+			super.afterExecute(r, t);
+		}
+		
+	}
+	
+	@Override
+	protected void terminated() {
+		try {
+			logger.info(String.format("Terminated: avg time = %d millis", totalTime.get() / numTasks.get()));
+		} finally {
+			super.terminated();
+		}
+	}
+	
+	
+
+}

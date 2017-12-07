7f1716cfcf176a01c5df94054cf804fdab5b6648
joker_xjh <1781934063@qq.com>
2017-12-07 18:28:55 +0800

    .

--- crawler/src/main/java/demo58/MoreObjects.java
+++ crawler/src/main/java/demo58/MoreObjects.java
@@ -75,5 +75,66 @@ public class MoreObjects {
 		
 		
 	}
+	
+	
+	public static String padStart(String string, int minLength, char padChar) {
+		if(string == null)
+			throw new NullPointerException();
+		if(string.length() >= minLength)
+			return string;
+		StringBuilder sb = new StringBuilder(minLength);
+		for(int i=string.length(); i<minLength; i++)
+			sb.append(padChar);
+		sb.append(string);
+		return sb.toString();
+	}
+	
+	public static String padEnd(String string, int minLength, char padChar) {
+		if(string == null)
+			throw new NullPointerException();
+		if(string.length() >= minLength)
+			return string;
+		StringBuilder sb = new StringBuilder(minLength);
+		sb.append(string);
+		for(int i=string.length(); i<minLength; i++)
+			sb.append(padChar);
+		return sb.toString();
+	}
+	
+	
+	public static String repeat(String string, int count) {
+		if(string == null)
+			throw new NullPointerException();
+		if(count == 1)
+			return string;
+		if(count == 0)
+			return "";
+		if(count < 0)
+			throw new IllegalArgumentException();
+		final int len = string.length();
+		final long longSize = (long)len * (long)count;
+		final int size = (int)longSize;
+		if(size != longSize)
+			throw new ArrayIndexOutOfBoundsException("Required array size too large: " + longSize);
+		final char[] array = new char[size];
+		string.getChars(0, len, array, 0);
+		int n;
+		for(n=len; n < size-n; n<<=1)
+			System.arraycopy(array, 0, array, n, n);
+		System.arraycopy(string, 0, array, n, size-n);
+		return new String(array);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }

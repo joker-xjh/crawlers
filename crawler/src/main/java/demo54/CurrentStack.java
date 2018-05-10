707fbdee2c32aa7484d3b112394ff4d139503380
joker_xjh <1781934063@qq.com>
2018-05-10 21:38:37 +0800

    并发栈

---
+++ crawler/src/main/java/demo54/CurrentStack.java
@@ -0,0 +1,33 @@
+package demo54;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CurrentStack<T> {
+	
+	private class Node {
+		T val;
+		Node next;
+	}
+	
+	private AtomicReference<Node> stack = new AtomicReference<>();
+	
+	public void push(T val) {
+		Node node = new Node();
+		node.val = val;
+		Node old = null;
+		do {
+			old = stack.get();
+			node.next = old;
+		} while (!stack.compareAndSet(old, node));
+	}
+	
+	public T pop() {
+		Node newTop = null, oldTop = null;
+		do {
+			oldTop = stack.get();
+			newTop = oldTop.next;
+		} while (!stack.compareAndSet(oldTop, newTop));
+		return oldTop.val;
+	}
+	
+}
